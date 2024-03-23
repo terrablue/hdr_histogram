1da3b2aed13f5b0004e2ca1b03902cf6323c5813
terrablue <102580937+terrablue@users.noreply.github.com>
2024-03-23 23:36:45 +0100

    initial commit

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    // Create static library
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "hdrhistogram",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibC();
+    lib.addCSourceFiles(&.{"hdr_histogram.c"}, &.{
+        "-std=c99",
+        "-Wall",
+        "-Os",
+        "-g",
+        "-DHDR_MALLOC_INCLUDE=\"hdr_redis_malloc.h\"",
+    });
+
+    b.installArtifact(lib);
+}
